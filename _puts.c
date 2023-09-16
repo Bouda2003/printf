ee0212dfe0119d8496eb51a189808de36c15ae20
marslinoED <lino.edward11@gmail.com>
2023-09-17 01:02:40 +0300

    alais

--- _puts.c
+++ _puts.c
@@ -9,6 +9,16 @@
 
 int _puts(const char *str)
 {
+	if (str == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return (6);
+	}
 	int cnt = 0;
 
 	while (*str != '\0')
