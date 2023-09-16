703cdab76556f3ca14d32955e856885258cd2866
marslinoED <lino.edward11@gmail.com>
2023-09-17 00:33:30 +0300

    alais

--- _puts.c
+++ _puts.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 /**
  * _puts - ll
@@ -10,11 +12,11 @@
 int _puts(const char *str)
 {
 	int count = 0;
+	int string_length = 0;
 
-	while (*str != '\0')
-	{
-		count += _putchar(*str);
-		str++;
-	}
-	return (count);
+	while (str[string_length] != '\0') {
+                    string_length++;
+                }
+	write(1, str, string_length);
+	return (string_length);
 }
