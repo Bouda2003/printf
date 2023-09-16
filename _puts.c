4864217a224573196a0f63b26dd69cabf3e3e678
marslinoED <lino.edward11@gmail.com>
2023-09-17 01:18:10 +0300

    alais

--- _puts.c
+++ _puts.c
@@ -9,6 +9,16 @@
 
 int _puts(const char *str)
 {
+	if (str == NULL)
+	{
+		str = "(null)";
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+			len++;
+		}
+		return (len);
+	}
 	int cnt = 0;
 
 	while (*str != '\0')
