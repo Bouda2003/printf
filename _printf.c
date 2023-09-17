34d077fc42605772fad27b0b98014258f0cf3553
marslinoED <lino.edward11@gmail.com>
2023-09-17 13:46:43 +0300

    commit 1

--- _printf.c
+++ _printf.c
@@ -1,54 +1,62 @@
-#include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
-#include <stdlib.h>
-
+#include "main.h"
 /**
  * _printf - printf function
- * @format: string that printed
- * Return: I DONT KNOW
+ * @format: Format string
+ * @...: Variable arguments
+ * Return: Number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	const char *formater;
-	va_list args;
+int as = 0;
+va_list g;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-	formater = format;
-	while (*formater != '\0')
-	{
-		if (*formater == '%')
-		{
-			formater++;
-			if (formater != NULL)
-			{
-				switch (*formater)
-				{
-					case '%':
-						count += _putchar('%');
-						break;
-					case 'c':
-						count += _putchar(va_arg(args, int));
-						break;
-					case 's':
-						count += _puts(va_arg(args, char*));
-						break;
-					default:
-						count++;
-						_putchar('%');
-						_putchar(*formater);
-						break;
-				}
-			}
-		}
-		else
-			count += _putchar(*formater);
-		formater++;
-	}
-	va_end(args);
-	return (count);
+va_start(g, format);
+
+while (*format)
+{
+if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
+{
+if (*(format + 1) == 'c')
+{
+char x = va_arg(g, int);
+write(1, &x, 1);
+as++;
 }
+else
+{
+char *sr = va_arg(g, char *);
+if (sr)
+{
+while (*sr)
+{
+write(1, sr++, 1);
+as++;
+}
+}
+else
+{
+write(1, "(null)", 6);
+as = as + 6;
+}
+}
+format = format + 2;
+}
+else if (*format == '%' && (*(format + 1) == '%' || *(format + 1) == '\0'))
+{
+write(1, "%", 1);
+as++;
+format = format + 2;
+}
+else
+{
+write(1, format++, 1);
+as++;
+}
+}
+
+va_end(g);
+return (as);
+}
+
--- g
+++ g
@@ -10,4 +10,3 @@ git add .
 git commit -m "$message"
 git push
 echo "$loober" > counter.txt
-
