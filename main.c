0cf39118362e214b8f66efaae0114d2a8efbd06f
marslinoED <lino.edward11@gmail.com>
2023-09-18 00:36:54 +0300

    alais

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 
  */
 int main(void)
 {
 	int cnt;
 
-	cnt = _printf("%d %i 1234 %c%c%c%c %s", 1234, 1234, '1', '2', '3', '4', "1234");
+	cnt = _printf("%c%c%c%c %s",'1', '2', '3', '4', "1234");
 	printf("\n%d\n", cnt);
 }
