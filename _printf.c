fdc7e36a28be8cc4dde7376785c2349204155516
Abdelrahman_Mohsen <aute.mohsen@gmail.com>
2023-09-17 12:19:02 -0700

    alais

--- _printf.c
+++ _printf.c
@@ -29,10 +29,10 @@ int _printf(const char *format, ...)
 					count += _putchar(va_arg(args, int));
 				else if (*format == 's' && (format != NULL))
 					count += _puts(va_arg(args, char*))
+				else
+					count += _putchar(*format);
+					format++;
 		}
-		else
-			count += _putchar(*format);
-		format++;
 	}
 	va_end(args);
 	return (count);
